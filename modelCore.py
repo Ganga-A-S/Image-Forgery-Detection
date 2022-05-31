f68a3a1eef3f6ad559607d247fa84f49f829d71f
gourigs <71514229+gourigs@users.noreply.github.com>
2022-05-31 15:11:48 +0530

    Update modelCore.py

--- modelCore.py
+++ modelCore.py
@@ -14,7 +14,7 @@ from keras.models import Model
 from keras.initializers import Constant
 from keras.constraints import Constraint
 from keras import backend as K
-from keras.layers.convolutional import Conv
+from keras.layers.convolutional import Conv2D
 from keras.legacy import interfaces
 from keras.engine import InputSpec
 import tensorflow as tf
@@ -23,7 +23,7 @@ import numpy as np
 #################################################################################
 # Model Utils for Image Manipulation Classification
 #################################################################################
-class Conv2DSymPadding( _Conv ) :
+class Conv2DSymPadding( _Conv2D ) :
     @interfaces.legacy_conv2d_support
     def __init__(self, filters,
                  kernel_size,
